8ae2edb61c87448a6e6d156b1ec6a0247e98b2ae
Dana1992sl <danamansl123@gmail.com>
2024-09-18 14:43:52 +0530

    itempg

--- src/pages/item/Item.jsx
+++ src/pages/item/Item.jsx
@@ -2,28 +2,42 @@ import React from 'react'
 import { products } from '../../assets/assets'
 import './Item.css'
 
+
 const Item = () => {
   return (
-    <div>
+    <>
+    <div className='item-container'>
     <div className="container">
         {products.map((item,index)=>{
             return (
                 <div  className="item" key={index}>
-                    <img src={item.image} />
+                    <img className='item-image' src={item.image} />
+                    
+                    <div className='item-info'>
                     <h2>{item.name}</h2>
-                    <p>{item.description}</p>
-                    <p>{item.category}</p>
-                    <p>{item.subCategory}</p>
-                    <p>{item.sizes}</p>
-                    <p>{item.date}</p>
-                    <p>{item.bestseller}</p>
-                    <button>Add to Cart</button>
+                    <p className='price'>${item.price}</p>
+                    <p className='discription'>{item.description}</p>
+                    <p className='select'>Select Size</p>
+                    <p className='sizes'>{item.sizes}</p>
+                    <button className='btn-cart'>Add to Cart</button>
+                    <hr className='horizontal'/>
+                    <div className='info'>
+                    <p>100% Original Product</p>
+                    <p>Cash on Delivery is available on this product</p>
+                    <p>Easy return and exchange policy within 7 Days</p></div>
+                    </div>
+                    
                 </div>
+                
             )
+            
         })}
+        
     </div>
+    
       
     </div>
+    </>
   )
 }
 
