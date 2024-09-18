32c8c7fcd81d6db4dbe123d78041ec2d82df5719
Dana1992sl <danamansl123@gmail.com>
2024-09-18 17:12:12 +0530

    itemnew edit

--- src/pages/item/Item.jsx
+++ src/pages/item/Item.jsx
@@ -4,6 +4,8 @@ import './Item.css'
 
 
 const Item = () => {
+  const size = ['S','M','L','XL','XXL']
+ 
   return (
     <>
     <div className='item-container'>
@@ -18,7 +20,14 @@ const Item = () => {
                     <p className='price'>${item.price}</p>
                     <p className='discription'>{item.description}</p>
                     <p className='select'>Select Size</p>
-                    <p className='sizes'>{item.sizes}</p>
+                    <div className='buttons'>
+                    
+                    <button className='sizes'>{size[0]}</button>
+                    <button className='sizes'>{size[1]}</button>
+                    <button className='sizes'>{size[2]}</button>
+                    <button className='sizes'>{size[3]}</button>
+                    <button className='sizes'>{size[4]}</button>
+                    </div>
                     <button className='btn-cart'>Add to Cart</button>
                     <hr className='horizontal'/>
                     <div className='info'>
