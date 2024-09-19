4988de65581cfe20c3e4a5833cc495accb8090dd
Dana1992sl <danamansl123@gmail.com>
2024-09-19 17:32:21 +0530

    cartnew

--- src/Context/context.jsx
+++ src/Context/context.jsx
@@ -4,19 +4,37 @@ export const storeContext = createContext(null);
 const storeContextProvider =(props)=>{
   const [shopItems,setShopItems] = useState({});
   const [products,setProducts]=useState([]);
-  const selectshop=(itemImage)=>{
-    if(!shopItems[itemImage]){
-      setShopItems((prev)=>({...prev, [itemImage]:itemImage}));
+  const addToCart=(itemId)=>{
+    if(!shopItems[itemId]){
+      setShopItems((prev)=>({...prev, [itemId]:1}));
     }else{
-      setShopItems((prev)=>({...prev, [itemImage]:prev[itemImage]}));
+      setShopItems((prev)=>({...prev, [itemId]:prev[itemId]+1}));
     }
   
   }
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in shopItems) {
+      if (shopItems[item]>0) {
+        let itemInfo = products.find((product) =>product._id === item);
+        totalAmount += itemInfo.price * shopItems[item];
+      }
+      
+    }
+    return totalAmount;
+  }
+  const removeFromCart =(itemId) => {
+    setShopItems((prev) => ({ ...prev, [itemId]: prev[itemId]-1 }));}
+
+
   const contextValue ={
     products,
     shopItems,
     selectshop,
-    setProducts
+    setProducts,
+    getTotalCartAmount,
+    addToCart,
+    removeFromCart
   }
   return (<storeContext.Provider value={contextValue}>
   {props.children}
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,6 +3,8 @@ import { assets, products } from '../../assets/assets'
 import './cart.css'
 
 const Cart = () => {
+  
+  
   return (
     <div className='cart'>
     <h1>Your Cart</h1>
@@ -11,6 +13,7 @@ const Cart = () => {
       return (
         <>
         <div className='cart-item' key={item.id}>
+        
           <img src={item.image} alt={item.name}/>
           <div>
             <h2 className='cart-item-name'>{item.name}</h2>
@@ -18,14 +21,35 @@ const Cart = () => {
             <p></p>
             <div className='cart-item-buttons'>
             <button className='bin'><img src={assets.bin_icon}/></button>
-            <button className='payment'>Payment</button>
+            
+            
             </div>
             <div className='cart-item-bottom'>
             <p className='cart-item-quantity'>Quantity: </p>
             <p className='cart-item-total'>Subtotal: ${item.price}</p>
           </div>
+          <div className='cart-bottom'>
+          <div className='cart-bottom-items'>
+          <h1>Cart Totals</h1>
+          <p></p>
+          <hr className='hori'/>
+          </div>
+          <div className='cart-bottom-items'>
+          <p>Subtotal</p>
+          <p></p>
+          <hr className='hori'/>
+          </div>
+          <p>Shipping Fee</p>
+          <hr className='hori'/>
+          <p>total</p>
+          <hr className='hori'/>
+          <button className='payment'>Payment</button>
+          
 
           </div>
+
+          </div>
+          
           
         </div>
         <hr/>
